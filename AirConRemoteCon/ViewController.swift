f3ad06722ca9b981ca321d0649b3483ac9d4711e
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-12-06 23:47:15 +0900

    Initial Commit

---
+++ AirConRemoteCon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirConRemoteCon
+//
+//  Created by 齋藤悠太 on 2019/12/06.
+//  Copyright © 2019年 齋藤悠太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
